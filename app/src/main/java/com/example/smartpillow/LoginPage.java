e7b7b84e7ccfd2cc27256695374e9323244b179c
Aia <AiaA07@github.com>
2025-10-30 12:52:12 -0400

    Added SignUp page

--- app/src/main/java/com/example/smartpillow/LoginPage.java
+++ app/src/main/java/com/example/smartpillow/LoginPage.java
@@ -1,13 +1,43 @@
 package com.example.smartpillow;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginPage extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        username = findViewById(R.id.UsernameText);
+        password = findViewById(R.id.PasswordText);
+        login = findViewById(R.id.Loginbtn);
+        signUp = findViewById(R.id.SignUpBtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //handle login
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //handle logic
+            }
+        });
+
+
     }
 }
