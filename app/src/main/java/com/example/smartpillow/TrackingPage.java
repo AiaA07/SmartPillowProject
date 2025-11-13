6a6bfdaa99323bcf4c8296bc1b45d13c30e89682
Aia <AiaA07@github.com>
2025-11-13 12:48:36 -0500

    updated tracking page button clicks

--- app/src/main/java/com/example/smartpillow/TrackingPage.java
+++ app/src/main/java/com/example/smartpillow/TrackingPage.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,11 @@ import java.util.Locale;
 
 public class TrackingPage extends AppCompatActivity {
 
+    private ImageView homeBtn3;
+    private ImageView statsBtn3;
+    private ImageView profileBtn3;
+
+
 
 
     @Override
@@ -23,6 +29,34 @@ public class TrackingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tracking_page);
 
+        homeBtn3 = findViewById(R.id.home3_Btn);
+        statsBtn3 = findViewById(R.id.stats3_Btn);
+        profileBtn3 = findViewById(R.id.profile3_Btn);
+
+        homeBtn3.setOnClickListener(v -> GotoHome3());
+        statsBtn3.setOnClickListener(v -> GoToStats3());
+        profileBtn3.setOnClickListener(v -> GoToProfile3());
 
     }
+
+    private void GotoHome3(){
+        Intent home3 =  new Intent(TrackingPage.this, HomePage.class);
+        startActivity(home3);
+    }
+
+    private void GoToStats3(){
+        Intent stats3 = new Intent(TrackingPage.this, StatsPage.class);
+        startActivity(stats3);
+    }
+
+    private void GoToProfile3(){
+        Intent profile3 = new Intent(TrackingPage.this, ProfilePage.class);
+        startActivity(profile3);
+    }
+
+
+
+
+
+
 }
